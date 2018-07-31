506ed1530827ebf610de7bbfe5cb4a963659705b
zhoufeifan <xieyang.zff@raycloud.com>
2018-07-31 11:13:47 +0800

    ..

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const url = require('./src/url');
-const throttle = require('./src/throttle');
+const other = require('./src/other');
 const validate = require('./src/validate');
 const keyCode = require('./src/keyCode');
 const dateTime = require('./src/dateTime');
 const device = require('./src/device');
 const object = require('./src/object');
-const utils = require('./src/utils');
 const amount = require('./src/amount');
 const Request = require('./src/request');
 
 module.exports = {
     url,
-    throttle,
+    other,
     validate,
     keyCode,
     dateTime,
     device,
     object,
-    utils,
     amount,
     Request
 };
