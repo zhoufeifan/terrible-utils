858df9c1da195b2909f068e693c253cde483d969
zhoufeifan <xieyang.zff@raycloud.com>
2018-10-20 09:53:01 +0800

    改进节流函数

--- src/other.js
+++ src/other.js
@@ -1,14 +1,39 @@
 /**
  *
- * @desc  函数节流
+ * @desc  先行函数节流
  * @param  {Function} fn 执行的目标函数
- * @param  {Number} delay 节流的的时间间隔
- * @param  {Number} maxDelay 触发行数执行的最大时间
+ * @param  {Number} delay 节流的时间间隔
  * @return {Function} 返回节流过的函数
  */
-function throttle(fn, delay, maxDelay) {
-    let timer = null;
+function throttleFirstInvoke(fn, delay) {
     let startTime;
+    return function () {
+        let context = this, args = arguments, currentTime = +new Date();
+        //如果不存触发时间，那么当前的时间就是触发时间
+        if (!startTime) {
+            startTime = currentTime;
+            fn.apply(context, args);
+        }
+        //控制在规定时间内只触发一次
+        if (currentTime - startTime >= delay) {
+            fn.apply(context, args);
+            startTime = currentTime;
+        }
+    };
+}
+
+/**
+ *
+ * @desc  后置函数节流
+ * @param  {Function} fn 执行的目标函数
+ * @param  {Number} delay 节流的时间间隔
+ * @param  {Number} maxDelay 触发函数执行的最大延迟时间，默认为0即立即执行
+ * @return {Function} 返回节流过的函数
+ */
+function throttleLastInvoke(fn, delay, maxDelay) {
+    let startTime;
+    let timer = null;
+    maxDelay = maxDelay || delay
     return function () {
         let context = this, args = arguments, currentTime = +new Date();
         //先清理上一次的调用触发（上一次调用触发事件不执行）
@@ -17,12 +42,12 @@ function throttle(fn, delay, maxDelay) {
         if (!startTime) {
             startTime = currentTime;
         }
-        //如果当前时间-触发时间大于最大的间隔时间（maxDelay），触发一次函数运行函数
+        //如果当前时间-触发时间大于maxDelay，触发一次函数运行函数
         if (currentTime - startTime >= maxDelay) {
             fn.apply(context, args);
             startTime = currentTime;
         }
-        //否则延迟执行
+        //否则延迟执行,即在maxDelay时间间隔内只触发一次函数执行
         else {
             timer = setTimeout(function () {
                 fn.apply(context, args);
@@ -30,4 +55,9 @@ function throttle(fn, delay, maxDelay) {
         }
     };
 }
-module.exports = throttle;
+
+
+module.exports = {
+    throttleFirstInvoke,
+    throttleLastInvoke
+};
--- test.js
+++ test.js
@@ -1,23 +1,15 @@
 require("babel-core/register");
 require("babel-polyfill");
 
-import {compress} from './src/image.js';
+import {throttleFirstInvoke,throttleLastInvoke} from './src/other.js';
 
-let input = document.getElementById('fileInput');
-let image = document.getElementById('image');
-input.addEventListener('change', async(e)=>{
-    let file = e.target.files[0];
-    try{
-        let blob = await compress(file,{
-            maxSize: 200*1024,
-            type: 'file'
-        });
-        image.onload = function(e) {
-            window.URL.revokeObjectURL(image.src); // 清除释放
-        };
-        console.log(blob);
-        image.src = window.URL.createObjectURL(blob);
-    }catch(e){
-        console.log(e);
-    }
-});
+var myFun = throttleFirstInvoke(()=>{
+    console.log('aa');
+},1000);
+
+var myFun1 = throttleLastInvoke(()=>{
+    console.log('bb');
+},1000);
+
+// window.onresize = myFun;
+window.onresize = myFun1;
